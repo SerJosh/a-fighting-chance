a9aa0ca755a7ef9d7989da42b3ab32dedb87a2c7
SerJosh <>
2023-08-28 21:59:43 +0000

    add innerHTML to empty box div and add id = (phone-screen) to div in phone div to change content on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 function update() {
     var gfg_down =
-        document.getElementById("removable");
-    gfg_down.remove();}
+        document.getElementById("phone-screen");
+    gfg_down.remove();
+
+    let emptyBox = document.getElementById('empty-box');
+    emptyBox.innerHTML = `<h2>How to Play</h2>
+  
+  
+  <br>
+  <button class="button-spacing" onclick="myRestore();">Back</button>`;
+}
